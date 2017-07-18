72547e000dc5753957112e3906f246e9d9060a20
Premor <spamerkik@gmail.com>
2017-07-18 16:52:34 +0300

    Добавил классы и функции

--- game.h
+++ game.h
@@ -1,11 +1,48 @@
-/*class newspaper {
+
+//мб общество и правительство отнаследовать от одного класса (пока не актуально)
+class nation;
+class society;
+class struct_ob
+{
+protected:
+	double status;// от 0 до 1000
+	double relation;//отношение к игроку, от -1000 до 1000
+public:
+	double get_stat();
+	double get_relation();
+	int set_stat(double);
+	int set_relation(double);
+	int change_stat(double);
+	int change_relation(double);
 	
-};*/
+};
 
-using namespace std;
+class newspaper { //собственно то, чем мы управляем в игре
+	int money;
+	vector<string> contracts;//с кем есть контракты
+public:
+	newspaper();
+	void weak_haras(nation*);
+	void weak_haras(society*);
+};
 
-class world{
-	time_t sec;
+class society:public struct_ob { //общественность
+	
+public:
+	society();
+	//friend newspaper;
+
+	
+};
+
+class nation:public struct_ob { //правительство
+public:
+	nation();
+	//friend newspaper;
+	
+};
+
+class world {
 	struct tm * times;
 	public:
 		world();
@@ -15,3 +52,4 @@ class world{
 		void next_day(int);
 };
 void end_day(world);
+void do_something(newspaper,society,nation);
